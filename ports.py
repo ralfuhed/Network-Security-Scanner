7e6452e0d92bec809206746bb6365276a02f3123
Rashed Al Fuhed <137357467+ralfuhed@users.noreply.github.com>
2024-02-07 16:35:15 -0500

    Copyrights ports.py

--- ports.py
+++ ports.py
@@ -1,5 +1,8 @@
-           ####################################
-                # Python Code #
+######################
+# ports.py
+# Author: Rashed Al Fuhed
+# Date: 10/01/2023
+######################
 
 import socket
 
