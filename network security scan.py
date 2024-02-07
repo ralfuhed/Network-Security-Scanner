b12ec2c92181767e1a2ae9dbceeda4301d212096
Rashed Al Fuhed <137357467+ralfuhed@users.noreply.github.com>
2024-02-07 16:25:29 -0500

    Add files via upload

---
+++ network security scan.py
@@ -0,0 +1,103 @@
+           ####################################
+                # Python Code #
+
+#### Port Scanner using Socket
+
+from socket import *
+import time
+startTime = time.time()
+
+if __name__ == '__main__':
+   target = input('Enter the host to be scanned: ')
+   t_IP = gethostbyname(target)
+   print ('Starting scan on host: ', t_IP)
+   
+   for i in range(50, 500):
+      s = socket(AF_INET, SOCK_STREAM)
+      
+      conn = s.connect_ex((t_IP, i))
+      if(conn == 0) :
+         print ('Port %d: OPEN' % (i,))
+      s.close()
+print('Time taken:', time.time() - startTime)
+
+
+            ## Port Scanner using ICMP (ping sweep)
+
+
+import os
+import platform
+
+from datetime import datetime
+net = input("Enter the Network Address: ")
+net1= net.split('.')
+a = '.'
+
+net2 = net1[0] + a + net1[1] + a + net1[2] + a
+st1 = int(input("Enter the Starting Number: "))
+en1 = int(input("Enter the Last Number: "))
+en1 = en1 + 1
+oper = platform.system()
+
+if (oper == "Windows"):
+   ping1 = "ping -n 1 "
+elif (oper == "Linux"):
+   ping1 = "ping -c 1 "
+else :
+   ping1 = "ping -c 1 "
+t1 = datetime.now()
+print ("Scanning in Progress:")
+
+for ip in range(st1,en1):
+   addr = net2 + str(ip)
+   comm = ping1 + addr
+   response = os.popen(comm)
+   
+   for line in response.readlines():
+      if(line.count("TTL")):
+         break
+      if (line.count("TTL")):
+         print (addr, "--> Live")
+         
+t2 = datetime.now()
+total = t2 - t1
+print ("Scanning completed in: ",total)
+
+
+         ### Port Scanner using TCP scan ###
+         
+import socket
+from datetime import datetime
+net = input("Enter the IP address: ")
+net1 = net.split('.')
+a = '.'
+
+net2 = net1[0] + a + net1[1] + a + net1[2] + a
+st1 = int(input("Enter the Starting Number: "))
+en1 = int(input("Enter the Last Number: "))
+en1 = en1 + 1
+t1 = datetime.now()
+
+def scan(addr):
+   s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+   socket.setdefaulttimeout(1)
+   result = s.connect_ex((addr,135))
+   if result == 0:
+      return 1
+   else :
+      return 0
+
+def run1():
+   for ip in range(st1,en1):
+      addr = net2 + str(ip)
+      if (scan(addr)):
+         print (addr , "is live")
+         
+run1()
+t2 = datetime.now()
+total = t2 - t1
+print ("Scanning completed in: " , total)
+
+
+
+        
